30f6ed83c94a5b2719d7b5370b236dc7aa25c95c
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-09-15 20:17:42 +0530

    Added: Employee wage

---
+++ src/com/bridgeLabz/EmployeeWage.java
@@ -0,0 +1,18 @@
+package com.bridgeLabz;
+
+
+    public class EmployeeWage {
+        public static void main(String[] args) {
+            //variables
+            int isFullTime = 1;
+            //use math random method
+            int empcheck = (int)(Math.random()*10%2);
+            //check emplyee prsent or not
+            if (empcheck == isFullTime){
+                System.out.println("employee is present");
+            }else {
+                System.out.println("employee not present");
+            }
+        }
+    }
+
